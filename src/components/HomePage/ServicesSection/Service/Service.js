38bda5ea0830ad7a5a2fb57eef9fac1b711e3442
TAMIM <saifur.tamim@gmail.com>
2021-10-18 14:50:20 +0600

    service card animation

--- src/components/HomePage/ServicesSection/Service/Service.js
+++ src/components/HomePage/ServicesSection/Service/Service.js
@@ -21,7 +21,7 @@ const Service = ({ service }) => {
                                 </p>)
                         }</div>
                         <button
-                            className="w-100 border-0 text-white px-4 py-2 rounded-3"
+                            className="service-btn w-100 border-0 text-white px-4 py-2 rounded-3"
                             style={{ background: themeColor }}>Learn More</button>
                     </div>
                     <div className="service-shadow" style={{ background: themeColor }}></div>
