0abaa65e9a4350a0841dcf778889cf2ab8858763
TAMIM <saifur.tamim@gmail.com>
2021-10-18 16:13:09 +0600

    navbar added
    navbar added and home banner slightly modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage';
 import Footer from './components/Shared/Footer/Footer';
+import Navbar from './components/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-      <Footer />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <div className="App">
+          <Switch>
+            <Route path="/home"><HomePage /></Route>
+            <Route path="/"><HomePage /></Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <header className="position-fixed top-0 w-100">
+            <nav className="navbar navbar-expand-lg navbar-light py-4 bg-light">
+                <div className="container">
+                    <NavLink to="/"
+                        className="navbar-brand d-flex justify-content-center me-5">
+                        <img src="/favicon.png" alt="" width="40" />
+                        <h1 className="fs-3 fw-bold m-0 ms-2 lh-base">Vigour</h1>
+                    </NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navLinks" aria-controls="navLinks" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navLinks">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item text-center">
+                                <NavLink className="nav-link fs-5" to="/home" activeStyle={{ color: '#f7565e' }}>Home</NavLink>
+                            </li>
+                            <li className="nav-item text-center">
+                                <NavLink className="nav-link fs-5" to="/contact" activeStyle={{ color: '#f7565e' }}>Contact</NavLink>
+                            </li>
+                        </ul>
+                        <div>
+                            <NavLink className="nav-link fs-5 text-center" to="/contact">
+                                <button className="btn text-white px-3" style={{ background: '#f7565e' }}>Login</button>
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 };
 
