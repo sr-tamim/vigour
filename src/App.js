8f58f022c167821b51fa1c9919a0318e8853af82
TAMIM <saifur.tamim@gmail.com>
2021-10-18 20:14:44 +0600

    athentication form designed
    login and sign up form created, designed and navbar background color slightly modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Shared/Footer/Footer';
 import Navbar from './components/Shared/Navbar/Navbar';
 import NotFound404 from './components/NotFound404';
 import AuthContext from './components/AuthContext/AuthContext';
-import Login from './components/AuthenticationPages/LoginPage/Login';
+import AuthenticationPages from './components/AuthenticationPages/AuthenticationPages';
 
 function App() {
   return (
@@ -18,7 +18,8 @@ function App() {
             <Switch>
               <Route path="/home"><HomePage /></Route>
               <Route exact path="/"><HomePage /></Route>
-              <Route path="/login"><Login /></Route>
+              <Route path="/login"><AuthenticationPages /></Route>
+              <Route path="/signup"><AuthenticationPages /></Route>
               <Route path="*"><NotFound404 /></Route>
             </Switch>
             <Footer />
--- src/components/AuthenticationPages/SignUpPage/SignUp.js
+++ src/components/AuthenticationPages/SignUpPage/SignUp.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import useUserContext from '../../../hooks/useUserContext';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <>
             <form className="text-center" >
                 <div className="form-heading fw-bold">
-                    <i className="fas fa-user"></i> <br />
-                    <span className="fs-2">Login</span>
+                    <i className="fas fa-user-plus"></i> <br />
+                    <span className="fs-2">Create Account</span>
                 </div>
                 {/* <div>{userError && handleFirebaseError(userError)}</div> */}
+                <div className="auth-form-input-wrapper">
+                    <input type="text" id="nameInput" placeholder="Name" required />
+                    <label>Name</label>
+                </div>
                 <div className="auth-form-input-wrapper">
                     <input type="email" id="emailInput" placeholder="Email" required />
                     <label>Email</label>
@@ -19,10 +23,10 @@ const Login = () => {
                     <input type="password" id="passwordInput" placeholder="Password" required />
                     <label>Password</label>
                 </div>
-                <input type="submit" className="submit-button text-white fs-5 px-4 py-2 border-0 rounded-3" value="Login" />
+                <input type="submit" className="submit-button text-white fs-5 px-4 py-2 border-0 rounded-3" value="Sign Up" />
             </form>
         </>
     );
 };
 
-export default Login;
+export default SignUp;
